94f12971c0515b37cfff9ddd8defb115d9ec6595
Lloyd Clark <lloyd.clark@sdlcsoftware.com>
2017-05-30 08:42:59 -0500

    Corrected dependency injection

---
+++ src/Psr7ControllerBundle/DependencyInjection/Psr7ControllerExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Psr7ControllerBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class Psr7ControllerExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__."/../Resources/config")
+        );
+        $loader->load("services.yml");
+    }
+}
